38b60bc4801333688fc965370e7f61996a9534b0
Hardik <hardikshah@gatech.edu>
2011-06-12 17:50:11 -0400

    Final commit

---
+++ db/migrate/20110311131251_change_community_rename_active_at_column.rb
@@ -0,0 +1,11 @@
+class ChangeCommunityRenameActiveAtColumn < ActiveRecord::Migration
+  def self.up
+   # remove_column :communities, :active_at
+   # add_column :communities, :activated_at, :datetime
+  end
+
+  def self.down
+   # remove_column :communities, :activate_at, :datetime
+   # add_column :communities, :actived_at
+  end
+end
